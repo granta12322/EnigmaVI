2e63ae4b2dcbb2a7037de8db2e2a5e806a696ea9
granta12322 <granta12322@gmail.com>
2022-09-08 02:30:45 +0100

    Add full testing skeleton

---
+++ src/components/rotorArray/rotorArray.test.js
@@ -0,0 +1,11 @@
+import { RotorArray} from "./rotorArray";
+
+test("Encryption symmetry", () => {
+
+})
+
+test("Rotors in array rotate at correct rates", () => {
+
+})
+
+test("Rotors reset correctly")
